f6e30159fddef1ebc907abe4c5febc67498a8f04
Rishav Handa <{focusedmed@gamil.com>
2026-05-04 15:19:44 +0530

    write dockerfile

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express();
+const PORT = 3000;
+app.get('/',(reg,res)=>{
+    res.send('Hello World');
+});
+
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+});
